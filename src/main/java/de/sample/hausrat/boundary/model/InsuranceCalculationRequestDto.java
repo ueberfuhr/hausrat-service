cac3e98b2b2b168ff98004efb8fdb8f9de051f87
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2021-06-14 01:10:57 +0200

    Remove unused import

--- src/main/java/de/sample/hausrat/boundary/model/InsuranceCalculationRequestDto.java
+++ src/main/java/de/sample/hausrat/boundary/model/InsuranceCalculationRequestDto.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
