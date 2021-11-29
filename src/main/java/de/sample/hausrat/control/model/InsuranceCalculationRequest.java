8c07197031b70c77b7a8ef6633a6c1f41fb93d59
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2021-11-29 14:11:33 +0100

    Add Spring Boot Test

--- src/main/java/de/sample/hausrat/control/model/InsuranceCalculationRequest.java
+++ src/main/java/de/sample/hausrat/control/model/InsuranceCalculationRequest.java
@@ -2,6 +2,7 @@ package de.sample.hausrat.control.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -10,12 +11,13 @@ import javax.validation.constraints.Positive;
  * A request to the {@link de.sample.hausrat.control.InsuranceCalculator}.
  */
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class InsuranceCalculationRequest {
 
     @NotNull
-    private final String product;
+    private String product;
     @Positive
-    private final double livingArea;
+    private double livingArea;
 
 }
